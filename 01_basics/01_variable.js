3cc0bc307581289f2d1706e40cda5c47e27c2ced
DisonDavis <disondavis@msn.com>
2024-11-16 10:47:27 +0000

    data type and conversion operation

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "disondavis@msnc.com"
+var accountPassword = "123hello123"
+accountCity = "Bangalore"
+
+//accountId = 2
+console.log(accountId)
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treate all JS code as newer version
+//alert("hello") //we are using  node js, not browser
+
+console.log(3 + 3)
+/* 
+    ecma website is for the official documentation
+    Mozila documentaion MDN
+*/
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean
+// null => standalone value, empty
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined)
+
